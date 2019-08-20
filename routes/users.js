926f9f110e9cd039968573e77a6ecc4a3cbe4234
Jonathan Joe <JonathanDonJoe@gmail.com>
2019-08-20 14:22:56 -0400

    Added pg-promise, env. Created database. Added POST to form to insert into database

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../db');
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -9,5 +11,17 @@ router.get('/', function(req, res, next) {
 // User is already at /users/ or else they wouldnt have gotten to this piece of middleware
 router.post('/registerProcess', (req, res) => {
   const {username, email, password, password2} = req.body;
+
+  const insertUserQuery = `
+  INSERT INTO users (username, email, password)
+  VALUES
+    ($1, $2, $3)
+  RETURNING id
+  `
+  db.one(insertUserQuery, [username, email, password]).then( resp => {
+    res.json({
+      msg: "useradded"
+    })
+  })
 });
 module.exports = router;
