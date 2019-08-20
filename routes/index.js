33d65c24455b047277151bdea54a2e1d42946a5b
Jonathan Joe <JonathanDonJoe@gmail.com>
2019-08-20 11:17:35 -0400

    Replaced titles with movie posters.  Added responsiveness

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const request = require('request');
 
 const apiKey = '1fb720b97cc13e580c2c35e1138f90f8';
 const apiBaseUrl = 'http://api.themoviedb.org/3';
+const imageBaseUrl = 'http://image.tmdb.org/t/p/w300';
 
 /* GET home page. */  
 router.get('/', function(req, res, next) {
@@ -12,8 +13,8 @@ router.get('/', function(req, res, next) {
   // request.get takes 2 args: 1. URL to get 2. the callback to run when req is fulfilled
   request.get(nowPlayingUrl, (error, response, movieData) => {
     const parsedData = JSON.parse(movieData);
-    console.log(parsedData);
-    res.render('index', { parsedData: parsedData.results });
+    // console.log(parsedData);
+    res.render('index', { parsedData: parsedData.results, imageBaseUrl });
   })
   // res.render('index', { title: 'Express' });
 });
